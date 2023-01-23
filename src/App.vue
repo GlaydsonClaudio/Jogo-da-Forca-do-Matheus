cf35ddce4363fd49994f996948a949b1c68fa913
Glaydson Claudio <glaydson.claudio@gmail.com>
2023-01-23 14:40:43 -0300

    Criação do teclado do jogo.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script setup>
+import { ref, computed } from 'vue'
 import HeaderForca from './components/HeaderForca.vue';
 import ImageForca from './components/ImageForca.vue';
 import WordForca from './components/WordForca.vue';
 import KeyboardForca from './components/KeyboardForca.vue';
 
+const right = ref(['A','D','X','G','R','J','E','K']);
+const wrong = ref(['Q','P','S','O']);
+const errors = computed(() => wrong.value.length);
 
-
-
+function teste(event) {
+  console.log(event);
+}
 
 </script>
 
 <template>
   <HeaderForca />
-  <ImageForca errors="0" />
+  <ImageForca :errors="errors" />
   <WordForca />
-  <KeyboardForca />
-
+  <KeyboardForca :right="right" :wrong="wrong" @letter-press="teste"/>
 </template>
 
 <style lang="scss">
--- src/components/KeyboardForca.vue
+++ src/components/KeyboardForca.vue
@@ -1,13 +1,53 @@
 <script setup>
-
+const props = defineProps(['right','wrong']);
+const keys = [
+    ['A','B','C','D','E','F','G'],
+    ['H','I','J','K','L','M'],
+    ['N','O','P','Q','R','S','T'],
+    ['U','V','W','X','Y','Z']
+];
+ 
+// console.log(props.letters);
 </script>
 <template>
-    <section class="keyboard-forca">
-    </section>
+<section class="keyboard-forca">
+    <div class="line-keys" v-for="line in keys">
+        <button 
+            class="keyboard" 
+            :class="{ 'success': right.includes(key), 'error': wrong.includes(key) }" 
+            v-for="key in line"
+            @click="$emit('letter-press',key)"
+        >
+            {{ key }}
+        </button>
+    </div>
+</section>
 </template>
 <style lang="scss">
 .keyboard-forca {
-    background-color: bisque;
     height: 35vh;
+
+    & .line-keys {
+        height: 25%;
+        display: flex;
+        justify-content: space-around;
+
+        & .keyboard {
+            width: 100%;
+            height: 100%;
+            background-color: lightgrey;
+            border: solid 1px gray;
+            font-size: 2rem;
+            color: #404040;
+
+            &.success {
+                background-color: green;
+            }
+
+            &.error {
+                background-color: red;
+            }
+        }
+    }
 }
 </style>
