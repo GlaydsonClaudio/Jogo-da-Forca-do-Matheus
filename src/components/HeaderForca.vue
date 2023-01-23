7c67a872bdc6e71c3080016513a43de29fece0bd
Glaydson Claudio <glaydson.claudio@gmail.com>
2023-01-23 15:33:51 -0300

    criação do cabecalho.

--- src/components/HeaderForca.vue
+++ src/components/HeaderForca.vue
@@ -2,13 +2,39 @@
 
 </script>
 <template>
-    <section class="header-forca">
-        
-    </section>
+<section class="header-forca">
+    <div class="title1">
+        Jogo da Forca
+    </div>
+    <div class="title2">
+        do
+    </div>
+    <div class="title3">
+        Matheus
+    </div>
+</section>
 </template>
 <style lang="scss">
 .header-forca{
-    background-color: red;
     height: 10vh;
+    display: flex;
+
+    & .title1 {
+        font-size: 2.5rem;
+        width: 6rem;
+        text-align: center;
+        
+    }
+
+    & .title2 {
+        font-size: 1.5rem;
+        display: flex;
+        align-items: center;
+    }
+
+    & .title3 {
+        font-size: 4.3rem;
+    }
+
 }
 </style>
